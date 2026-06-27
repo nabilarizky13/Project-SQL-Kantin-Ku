30c7abc4d5b041f1b433924243bb7c8feb0747b0
nabilarizky13 <nabilarizkyilahi@gmail.com>
2026-06-27 14:43:59 +0700

    fix: force IPv4 connection to Supabase pooler

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
-    }
+    },
+    family: 4  // Force IPv4 agar tidak timeout di IPv6
 });
 
 module.exports = pool;
